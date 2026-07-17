391a244afaf6307bd47144cc861937d9876d7e88
Madara-Uchiha-001 <121989958+Madara-Uchiha-001@users.noreply.github.com>
2026-07-17 18:33:50 +0530

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+client = MongoClient(os.environ["MONGO_URI"])
+db = client["Cluster0"]
+settings_col = db["settings"]
+
+@app.route("/f/<code>")
+def forward(code):
+    doc = settings_col.find_one({"_id": "config"})
+    active_bot = doc["active_bot"] if doc else "roxyrobot"
+    return redirect(f"https://t.me/{active_bot}?start={code}", code=302)
