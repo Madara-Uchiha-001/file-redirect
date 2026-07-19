85714e8d9bdf054d6cb51c0a834d7f32dff372f1
Madara-Uchiha-001 <121989958+Madara-Uchiha-001@users.noreply.github.com>
2026-07-19 12:03:03 +0530

    Change default active_bot to 'gentlebladebot'

--- api/index.py
+++ api/index.py
@@ -10,5 +10,5 @@ settings_col = db["settings"]
 @app.route("/f/<code>")
 def forward(code):
     doc = settings_col.find_one({"_id": "config"})
-    active_bot = doc["active_bot"] if doc else "roxyrobot"
+    active_bot = doc["active_bot"] if doc else "gentlebladebot"
     return redirect(f"https://t.me/{active_bot}?start={code}", code=302)
